d94fcdfd51d2a278ac38163bc5e8c6a949cfcd23
luongvinhthao <luongvinhthao@gmail.com>
2014-08-27 10:13:46 +0700

    edit ssh password

--- openstack-status.sh
+++ openstack-status.sh
@@ -3,37 +3,39 @@ set -e
 echo "------------------------------Check status of openstack service ---------------------------------"
 echo "=============================Controller Node============================="
 echo "Check nova service ============================="
-		if [ echo `service nova-api status | grep -q start` ]
+ 
+
+		if [[ `service nova-api status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "nova-api \t\t\t\t active"
 			else
 			        echo -e "nova-api \t\t\t\t deactive"
 		fi
-		if [ echo `service nova-cert status | grep -q start` ]
+		if [[ `service nova-cert status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "nova-cert \t\t\t\t active"
 			else
 			        echo -e "nova-cert \t\t\t\t deactive"
 		fi
-		if [ echo `service nova-conductor status | grep -q start` ]
+		if [[ `service nova-conductor status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "nova-conductor \t\t\t\t active"
 			else
 			        echo -e "nova-conductor \t\t\t\t deactive"
 		fi
-		if [ echo `service nova-cert status | grep -q start` ]
+		if [[ `service nova-consoleauth status | grep -c start` -eq 1 ]]
 			then
-			        echo -e "nova-conductor \t\t\t\t active"
+			        echo -e "nova-consoleauth \t\t\t\t active"
 			else
-			        echo -e "nova-conductor \t\t\t\t deactive"
+			        echo -e "nova-consoleauth \t\t\t\t deactive"
 		fi
-		if [ echo `service nova-novncproxy status | grep -q start` ]
+		if [[ `service nova-novncproxy status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "nova-novncproxy \t\t\t active"
 			else
 			        echo -e "nova-novncproxy \t\t\t deactive"
 		fi
-		if [ echo `service nova-scheduler status | grep -q start` ]
+		if [[ `service nova-scheduler status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "nova-scheduler \t\t\t\t active"
 			else
@@ -42,47 +44,47 @@ echo "Check nova service ============================="
 	
 echo "Check glance service ============================="
 
-	if [ echo `service glance-registry status | grep -q start` ]
+	if [[  `service glance-registry status | grep -c start` -eq 1 ]]
 		then
 		        echo -e "glance-registry \t\t\t active"
 		else
 		        echo -e "glance-registry \t\t\t deactive"
 	fi
 
-	if [ echo `service glance-api status | grep -q start` ]
+	if [[  `service glance-api status | grep -c start` -eq 1 ]]
 		then
 		        echo -e "glance-api \t\t\t\t active"
 		else
 		        echo -e "glance-api \t\t\t\t deactive"
 	fi
 echo "Check keystone service ============================="
-	if [ echo `service keystone status | grep -q start` ]
+	if [[  `service keystone status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "keystone \t\t\t\t active"
 			else
 			        echo -e "keystone \t\t\t\t deactive"
 	fi
 echo "Check horizon service ============================="
-	if [ echo `service apache2 status | grep -q start` ]
+	if [[ `service apache2 status | grep -c running` -eq 1 ]]
 			then
 			        echo -e "apache2 \t\t\t\t active"
 			else
 			        echo -e "apache2 \t\t\t\t deactive"
 	fi
-	if [ echo `service memcached status | grep -q start` ]
+	if [[ `service memcached status | grep -c running` -eq 1 ]]
 			then
 			        echo -e "memcached \t\t\t\t active"
 			else
 			        echo -e "memcached \t\t\t\t deactive"
 	fi
 echo "Check cinder service ============================="
-	if [ echo `service cinder-scheduler status | grep -q start` ]
+	if [[  `service cinder-scheduler status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "cinder-scheduler \t\t\t active"
 			else
 			        echo -e "cinder-scheduler \t\t\t deactive"
 	fi
-	if [ echo `service cinder-api status | grep -q start` ]
+	if [[  `service cinder-api status | grep -c start`  -eq 1 ]]
 			then
 			        echo -e "cinder-api \t\t\t\t active"
 			else
@@ -91,14 +93,14 @@ echo "Check cinder service ============================="
 
 
 echo "Check swift service ============================="
-	if [ echo `service swift-proxy status | grep -q start` ]
+	if [[  `service swift-proxy status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "swift-proxy \t\t\t\t active"
 			else
 			        echo -e "swift-proxy \t\t\t\t deactive"
 	fi
 echo "Check neutron service ============================="
-	if [ echo `service neutron-server status | grep -q start` ]
+	if [[  `service neutron-server status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "neutron-server \t\t\t\t active"
 			else
@@ -106,53 +108,78 @@ echo "Check neutron service ============================="
 	fi
 
 echo "=============================Compute01 Node============================="
-	if [ echo `ssh root@172.22.22.181 service nova-api-metadata status | grep -q start` ]
+	if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.181 service nova-api-metadata status | grep -c start` -eq 1 ]]
         then
             echo -e "nova-api-metadata \t\t\t\t active"
         else
             echo -e "nova-api-metadata \t\t\t\t deactive"
     fi
-	if [ echo `ssh root@172.22.22.181 service  nova-compute  status | grep -q start` ]
+	if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.181 service  nova-compute  status | grep -c start` -eq 1 ]]
 	    then
 	            echo -e "nova-compute  \t\t\t\t active"
 	    else
 	            echo -e "nova-compute  \t\t\t\t deactive"
 	fi
-	if [ echo `ssh root@172.22.22.181 service  nova-network status | grep -q start` ]
+	if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.181 service  nova-network status | grep -c start` -eq 1 ]]
         then
             echo -e "nova-network \t\t\t\t active"
         else
             echo -e "nova-network \t\t\t\t deactive"
+    fi
+    if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.181 service  neutron-ovs-cleanup  status | grep -c start` -eq 1 ]]
+        then
+            echo -e "neutron-ovs-cleanup  \t\t\t\t active"
+        else
+            echo -e "neutron-ovs-cleanup  \t\t\t\t deactive"
+    fi
+    if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.181 service  neutron-plugin-openvswitch-agent status | grep -c start` -eq 1 ]]
+        then
+            echo -e "neutron-plugin-openvswitch-agent \t\t\t\t active"
+        else
+            echo -e "neutron-plugin-openvswitch-agent \t\t\t\t deactive"
     fi
 echo "=============================Network Node============================="
-	if [ echo `ssh root@172.22.22.184 service nova-api-metadata status | grep -q start` ]
+	if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.184 service openvswitch-switch status | grep -c start` -eq 1 ]]
         then
-            echo -e "nova-api-metadata \t\t\t\t active"
+            echo -e "openvswitch-switch \t\t\t\t active"
         else
-            echo -e "nova-api-metadata \t\t\t\t deactive"
+            echo -e "openvswitch-switch \t\t\t\t deactive"
     fi
-	if [ echo `ssh root@172.22.22.181 service  nova-compute  status | grep -q start` ]
+	if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.184 service  neutron-plugin-openvswitch-agent  status | grep -c start` -eq 1 ]]
 	    then
-	            echo -e "nova-compute  \t\t\t\t active"
+	            echo -e "neutron-plugin-openvswitch-agent  \t\t\t\t active"
 	    else
-	            echo -e "nova-compute  \t\t\t\t deactive"
+	            echo -e "neutron-plugin-openvswitch-agent  \t\t\t\t deactive"
 	fi
-	if [ echo `ssh root@172.22.22.181 service  nova-network status | grep -q start` ]
+	if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.184 service  neutron-l3-agent status | grep -c start` -eq 1 ]]
         then
-            echo -e "nova-network \t\t\t\t active"
+            echo -e "neutron-l3-agent \t\t\t\t active"
         else
-            echo -e "nova-network \t\t\t\t deactive"
+            echo -e "neutron-l3-agent \t\t\t\t deactive"
+    fi
+    if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.184 service  neutron-metadata-agent status | grep -c start` -eq 1 ]]
+        then
+            echo -e "neutron-metadata-agent \t\t\t\t active"
+        else
+            echo -e "neutron-metadata-agent \t\t\t\t deactive"
+    fi
+    if [[  `ssh -i /root/.ssh/id_rsa root@172.22.22.184 service  neutron-dhcp-agent status | grep -c start` -eq 1 ]]
+        then
+            echo -e "neutron-dhcp-agent \t\t\t\t active"
+        else
+            echo -e "neutron-dhcp-agent \t\t\t\t deactive"
     fi
 echo "=============================Cinder Node============================="
-	if [ echo `service cinder-volume status | grep -q start` ]
+	if [[  `service cinder-volume status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "cinder-volume \t\t\t active"
 			else
 			        echo -e "cinder-volume \t\t\t deactive"
 	fi
-	if [ echo `service tgt  status | grep -q start` ]
+	if [[  `service tgt  status | grep -c start` -eq 1 ]]
 			then
 			        echo -e "tgt  \t\t\t\t active"
 			else
 			        echo -e "tgt  \t\t\t\t deactive"
 	fi
+	
